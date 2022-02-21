a56091de6f8ae2dfe7a5e978af175890fb8a3f1a
Niko Lazur <lazur.niko@gmail.com>
2022-02-21 21:17:35 +0200

    added tests

--- specs/listOne.spec.ts
+++ specs/listOne.spec.ts
@@ -6,8 +6,11 @@ Scenario('get Backend/Api list', ({ I }) => {
   I.getNodeElement();
 
   I.amOnPage('/')
-  I.seeElement('//section[4]/div[last()]')
-  within('//section[4]/div[last()]', () => {
+  I.seeElement('form[role="search"]')
+  I.fillField('[placeholder="Search for your technology..."]', 'node')
+  I.see('Backend/API')
+  I.seeElement('[href="https://auth0.com/docs/quickstart/backend/nodejs"]')
+  within('[href="https://auth0.com/docs/quickstart/backend/nodejs"]', () => {
   I.see('Node (Express) API');
   })
 });
